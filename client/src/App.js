a5f03e125389bc110113606452651a020234e063
Marcel Durgante Dev <imfo@marceldurgante.dev>
2022-08-05 07:26:12 +0200

    Routes file added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import * as route from './routes';
 import Home from './pages/home/Home';
 
 function App() {
-  
   return (
-        <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path={route.HOME} element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
